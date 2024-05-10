abdfb93e0979a74510ebd9d466b8439faf3f8f8d
Guy Cohen <guyco2204@gmail.com>
2024-05-10 18:24:42 +0300

    python symnmf WIP

--- symnmf.py
+++ symnmf.py
@@ -2,17 +2,17 @@ import sys
 import math
 import numpy as np
 import pandas as pd
-from numpy.linalg import matrix_power
+from scipy.linalg import fractional_matrix_power
 
-MAX_ITER = 200
-EPSILON = 0.001
+MAX_ITER = 300
+EPSILON = 1e-4 # 0.0001
 BETA = 0.5
 GOAL_PARAMS = ['symnmf', 'sym', 'ddg', 'norm']
 
 
 def main():
 
-    if len(sys.argv != 4):
+    if len(sys.argv) != 4:
         print("please enter all 3 arguments")
         exit()
 
@@ -22,83 +22,127 @@ def main():
 
     # number of required clusters
     k = convert_to_number(sys.argv[1])
-    
+
     # goal can be: (symnmf | sym | ddg | norm)
     goal = sys.argv[2]
     assert goal in GOAL_PARAMS, "goal is incorrect! can be only: 'symnmf', 'sym', 'ddg', 'norm'"
-    
+
     file_name = sys.argv[3]
+    print('file_name: ' + file_name)
     data_table = pd.read_csv(file_name, header=None)
+    print("data_table: \n" + str(data_table))
     X = data_table.to_numpy()
+    N, d = np.shape(X)
+
+    print("X: \n" + str(X))
 
     # STEP b:
     # STEP 1.1 - The Similarity Matrix:
     A = get_similarity_matrix(X)
 
+    print("A: \n" + str(A))
+
     # STEP 1.2 - The diagonal degree Matrix:
     D = get_diagonal_matrix(A)
 
+    print("D: " + str(D))
+
     # STEP 1.3 - The normalized similarity matrix:
-    tmp = matrix_power(D, -0.5)  # TODO: rename tmp variable
-    W = tmp * A * tmp
+    tmp = fractional_matrix_power(D, -0.5)
+
+    print("tmp: " + str(tmp))
+
+    W = np.matmul(np.matmul(tmp, A), tmp)
+
+    print("W : " + str(W))
 
     # STEP 1.4 - Algorithm for optimizing H:
 
     # STEP 1.4.1 - Initialize H:
     m = np.mean(W)
-    H = np.full_like(W, fill_value=np.random.uniform(0, 2 * math.sqrt(m / k)))
+    H = np.random.uniform(low=0, high=np.random.uniform(0, 2 * math.sqrt(m / k)), size=(N, k))
+    # H = np.full_like(W, fill_value=np.random.uniform(0, 2 * math.sqrt(m / k)))
+    
+    print("randomized H-" + str(H))
 
     # STEP 1.4.2 - Update H:
     diff = 1
-    iter = 0
+    iter = 1
     while diff >= EPSILON and iter <= MAX_ITER:
-        next_H = H * (1 - BETA + BETA*((W*H) / (H*H.transpose()*H)))
+        next_H = H * (1 - BETA + BETA*(np.matmul(W, H) /
+                                       np.matmul(np.matmul(H, np.transpose(H)), H)))
+        
+        print("iter - " + str(iter))
+        print("H - " + str(H))
+        print("next_H - " + str(next_H))
 
-        diff = euclidean_distance(next_H, H)
+        diff = get_frobenius_norm(next_H - H) ** 2
+        iter += 1
+        H = next_H
+    
+    print("H -" + str(H))
 
     # STEP 1.5 - Deriving a clustering solution
-    vectors, clusters = np.shape(H)
-    # for vector in vectors
+    
+    # clusters = np.empty((k, N))
+    # print("X - " + str(X))
+    
+    # for i in range(N):
+    #     max_index = np.argmax(H[i])
+    #     print("max_index - " + str(max_index))
+        
+    #     np.append(clusters[max_index], X[i])
+    #     # clusters[max_index].append(X[i])
+    
+
+    # print("clusters - " + str(clusters))
 
 def get_diagonal_matrix(A):
     rows, columns = np.shape(A)
     D = np.empty_like(A)
 
-    for i in rows:
-        for j in columns:
+    for i in range(rows):
+        for j in range(columns):
 
             if i != j:
                 D[i][j] = 0
 
             else:
-                D[i][j] = np.sum(A, axis=0)
+                D[i][j] = np.sum(A, axis=1)[i]
 
     return D
 
 
 def get_similarity_matrix(X):
-    rows, columns = np.shape(X)
-    A = np.empty_like(X)
+    rows, _columns = np.shape(X)
+    print("rows - " + str(rows))
+    A = np.empty(shape=(rows, rows))
 
-    for i in rows:
-        for j in columns:
+    for i in range(rows):
+        for j in range(rows):
 
             if i == j:
                 A[i][j] = 0
 
             else:
-                A[i][j] = math.exp(-(euclidean_distance(X[i], X[j]) ^ 2) / 2)
+                A[i][j] = math.exp(-(euclidean_distance(X[i], X[j]) ** 2) / 2)
 
     return A
 
 
 def euclidean_distance(vector_x, vector_y):
     sum = 0
-    for i in range(vector_x):
-        sum += (vector_x[i] - vector_y[i]) ^ 2
+    for i in range(len(vector_x)):
+        sum += (vector_x[i] - vector_y[i]) ** 2
     return math.sqrt(sum)
 
+
+def get_frobenius_norm(H):
+    return math.sqrt(np.trace(H*H))
+
 # Function to convert a string to int
+
+
 def convert_to_number(str):
     try:
         return int(str)
@@ -106,6 +150,7 @@ def convert_to_number(str):
         print(str + " is not a whole number!")
         print("Check your arguments again")
         exit()
+
 
 if __name__ == "__main__":
     main()
