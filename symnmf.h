1a358558c8905ec74700b4e94e80978fd573c498
Guy Cohen <guyco2204@gmail.com>
2024-05-20 16:22:02 +0300

    feat(makefile):

--- symnmf.h
+++ symnmf.h
@@ -1,14 +1,14 @@
 #ifndef CAP_H
 #define CAP_H
 
-void get_similarity_matrix(double **X, double **A, int n);
+void get_similarity_matrix(double **X, double **A, int n, int d);
 void get_diagonal_degree_matrix(double **A, double **D, int n);
 void get_normalized_similarity_matrix(double **A, double **D, double ***W, int n);
 void get_clusters(double **W, double **H, int n, int k);
 
 double squared_euclidean_distance(double *point1, double *point2, int d);
 double row_sum(double **A, int i, int n);
-void allocate_matrix(double **mat, int n, int d);
+void allocate_matrix(double ***mat, int n, int d);
 void print_matrix(double **mat, int n, int d);
 
 #endif
